507bef3eb0f7308f3b0c2ddc27ae250c4180f99c
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-08-08 20:47:48 -0400

    Recycler view and adapter linked

--- app/src/main/java/com/example/ibtesamm/audiobookplayer/MainActivity.java
+++ app/src/main/java/com/example/ibtesamm/audiobookplayer/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.ibtesamm.audiobookplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView chapterView;
     Button playButton;
     SeekBar playBack;
+
+    //Support
+    ChapterAdapter chapterAdapter;
+    ArrayList<ChapterInfo> chapters;
 
 
     @Override
@@ -23,6 +31,32 @@ public class MainActivity extends AppCompatActivity {
         chapterView = findViewById(R.id.chapterView);
         playBack = findViewById(R.id.playBack);
         playButton = findViewById(R.id.play);
+
+        //Defines the chapters list
+        chapters = new ArrayList<>();
+
+        defineChpaters();
+
+    }
+
+    //Adds the chapter cards to the recycler view
+    private void defineChpaters(){
+
+        //Sets the orientarion
+        chapterView.setLayoutManager(new LinearLayoutManager(this) );
+
+        //Creates the adapter
+        ChapterAdapter chapterAdapter = new ChapterAdapter(this, chapters);
+
+        //Defines that the play buttons on the cards do
+        chapterAdapter.setOnButtonClickListener(new ChapterAdapter.onButtonClickListener() {
+            @Override
+            public void onButtonClick(Button b, View v, ChapterInfo c, int pos) {
+                //When button clicked
+            }
+        });
+
+        chapterView.setAdapter(chapterAdapter); //Sets the adapter to the recycler view and shows the cards
 
     }
 }
