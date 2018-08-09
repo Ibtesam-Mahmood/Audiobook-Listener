93bd33f9b9737f757f074c9b6a4df8072190f276
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-08-08 20:42:53 -0400

    View elements defined in main

--- app/src/main/java/com/example/ibtesamm/audiobookplayer/MainActivity.java
+++ app/src/main/java/com/example/ibtesamm/audiobookplayer/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.ibtesamm.audiobookplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    //View Elements
+    RecyclerView chapterView;
+    Button playButton;
+    SeekBar playBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Views defined
+        chapterView = findViewById(R.id.chapterView);
+        playBack = findViewById(R.id.playBack);
+        playButton = findViewById(R.id.play);
+
     }
 }
